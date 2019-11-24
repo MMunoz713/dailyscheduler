aad036bec44d0f4f356f51cc71b948c241061743
MMunoz713 <odst.mrtn@gmail.com>
2019-11-24 17:14:29 -0600

    push to pull to desktop.

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-var currentDay = moment().format("MMMM Do YYYY")
-$("#currentDay").text(currentDay)
+var currentDay = moment().format("MMMM Do YYYY");
+$("#currentDay").text(currentDay);
+
+$(document).ready(function(){
+    $('div.col-sm-2').css({
+            'height':'200px',
+            'width': '200px',
+            'border-radius':'10px',
+            'border-color':'pink',
+            'border-width': '2em'});
+});
