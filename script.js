e6d026c693262feeaa9abfc515c3f1c859e60bd3
smarties713 <odst.mrtn@gmail.com>
2019-11-27 15:56:12 -0600

    added background image and color changes to the time.

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-var currentDay = moment().format("MMMM Do YYYY");
+var currentDay = moment().format("MMMM Do YYYY, h:mm");
 $("#currentDay").text(currentDay);
+
+
+var curTime = moment().format("H");
+$(".time").each(function(index, value){
+  console.log(index)
+ var time = moment($(this).text(),"hA").format("H");
+ if (time < 10) {
+   time = "0"+ time;
+ }
+ console.log(time)
+ if(time < curTime)
+ {
+  $(this).css("backgroundColor","darkgray");
+ }
+ else if (time > curTime ){
+  $(this).css("backgroundColor","limegreen");
+ }
+ else {
+   $(this).css("backgroundColor","red")
+ }
+})
 
