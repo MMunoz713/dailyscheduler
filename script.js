5a0ea34ecb6f5ba85d38335fea62dcbfd30542b0
smarties713 <odst.mrtn@gmail.com>
2019-11-27 13:36:44 -0600

    date function added

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 var currentDay = moment().format("MMMM Do YYYY");
 $("#currentDay").text(currentDay);
 
-$(document).ready(function(){
-    $('div.col-sm-2').css({
-            'height':'200px',
-            'width': '200px',
-            'border-radius':'10px',
-            'border-color':'pink',
-            'border-width': '2em'});
-});
