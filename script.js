f28afa8b236296e7b304d78e90014e1c386dd53d
MMunoz713 <odst.mrtn@gmail.com>
2019-11-27 21:43:38 -0600

    final touches

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 var currentDay = moment().format("MMMM Do YYYY, h:mm");
 $("#currentDay").text(currentDay);
-
-var todoList = document.querySelector("#todo-list");
 var hours = ["NineAM", "TenAM", "ElevenAM", "TwelvePM", "OnePM", "TwoPM", "ThreePM", "FourPM", "FivePM"];
-
 var curTime = moment().format("H");
-$(".time").each(function(index, value){
+
+// Changes background color of time blocks depeneding on the hour.
+$(".time").each(function (index, value) {
   console.log(index)
- var time = moment($(this).text(),"hA").format("H");
- if (time < 10) {
-   time = "0"+ time;
- }
- console.log(time)
- if(time < curTime)
- {
-  $(this).css("backgroundColor","darkgray");
- }
- else if (time > curTime ){
-  $(this).css("backgroundColor","limegreen");
- }
- else {
-   $(this).css("backgroundColor","red")
- }
+
+  var time = moment($(this).text(), "hA").format("H");
+  if (time < 10) {
+    time = "0" + time;
+  }
+
+  console.log(time);
+
+  if (time < curTime) {
+    $(this).css("backgroundColor", "darkgray");
+  }
+  else if (time > curTime) {
+    $(this).css("backgroundColor", "limegreen");
+  }
+  else {
+    $(this).css("backgroundColor", "red");
+  }
 })
 
 // This function retrieves the values from the text input and saves it to its specified hour.
-function store(hour){
+function store(hour) {
   var textFieldInput = document.getElementById("textField" + hour);
   localStorage.setItem(hour, textFieldInput.value);
   console.log("Stored value for: " + hour + " : " + textFieldInput.value);
 }
 
+// Loops through hours array to find stored values. This function gets called everytime the app runs.
 function init() {
   for (i = 0; i < hours.length; i++) {
     var hour = hours[i];
@@ -40,4 +42,5 @@ function init() {
   console.log("App Initialized");
 }
 
+// Runs 'init' function
 init();
